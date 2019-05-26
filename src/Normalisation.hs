c973a6979e135c11c5dbccd0ea0bdbcafa2f75b6
trex <adamjking1995@outlook.com>
2019-05-26 14:31:37 +0100

    Improving test coverage.

--- src/Normalisation.hs
+++ src/Normalisation.hs
@@ -1,26 +1,24 @@
 module Normalisation
-  ( rangeFrom
-  , normalise
+  ( normalise
   )
 where
 
-import           Data.List                      ( minimum
-                                                , maximum
-                                                )
 
-import Fmt
-
-rangeFrom :: (Ord n) => [n] -> (n, n)
-rangeFrom xs = (minimum xs, maximum xs)
+diff :: Num a => (a, a) -> a
+diff (a, b) = b - a
 
 normalise :: (Ord a, Fractional a, Show a) => (a, a) -> (a, a) -> a -> a
-normalise _ (0, 0) _ = 0
-normalise from @ (l, h) to @ (l', h') i 
-  | to == from = i
-  | l == h = noBaseRangeErr
-  | otherwise  = calc
- where
-  diffA    = h - l
-  diffB    = h' - l'
-  calc     = (((i - l) / diffA) * diffB) + l' 
-  noBaseRangeErr = error $ "Cannot normalise without a concept of a base range (" +|| from ||+ ")."
+normalise origin target value
+  | origin == target = value
+  | 0 == diff target = fst target
+  | 0 == diff origin = error
+    "Cannot normalise a value that has no minimum and maximum value."
+  | let (l, h) = origin in (l > value || h < value) = error
+    "Cannot normalise a value outside of the given range."
+  | otherwise = normalise' origin target value
+
+normalise' :: (Fractional a, Num a) => (a, a) -> (a, a) -> a -> a
+normalise' origin target value =
+  let originWidth = diff origin
+      targetWidth = diff target
+  in  (((value - fst origin) / originWidth) * targetWidth) + fst target
--- test/NormalisationSpec.hs
+++ test/NormalisationSpec.hs
@@ -1,31 +1,60 @@
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module NormalisationSpec where
 
 import           Normalisation
-import           ArbitraryInstances
 import           Test.QuickCheck
 import           Test.Hspec.QuickCheck
 import           Test.Hspec                    as HS
+import           CommonProperties
+
+factor :: Gen Double
+factor = getPositive <$> arbitrary
 
 spec :: HS.Spec
 spec = describe "normalise" $ do
     prop "has no effect when normalising to the same range" $ do
-        Range { lower = l, higher = h } <- arbitrary :: Gen (Range Double)
-        i                               <- choose (l, h)
-        let outcome = normalise (l, h) (l, h) i
-        return (i == outcome)
+        (l, h) <- range @Double
+        i      <- choose (l, h)
+        return (i === normalise (l, h) (l, h) i)
 
     prop
-            "produces a value x times larger when the second range is x times larger"
-        $ \(Positive f :: Positive Double, Range { lower = l, higher = h }) ->
-              forAll (choose (l, h)) $ \v ->
-                  let outcome = normalise (l, h) (l * f, h * f) v
-                  in  (1e-3 > abs (outcome - (v * f)))
+            "a range that is linearly smaller/bigger by a factor f is the same as doing ( f * that value )"
+        $ do
+              f      <- factor
+              (l, h) <- range
+              v      <- choose (l, h)
+              let outcome = normalise (l, h) (l * f, h * f) v
+              return (1e-3 > abs (outcome - (v * f)))
 
     prop
-            "produces a value x times larger when the second range is x times smaller"
-        $ \(Positive f :: Positive Double, Range { lower = l, higher = h }) ->
-              forAll (choose (l, h)) $ \v ->
-                  let outcome = normalise (l, h) (l / f, h / f) v
-                  in  (1e-3 > abs (outcome - (v / f)))
+            "if the target range has no actual range than the normalised value is that number (ie normalising to (10, 10) always results in 10)"
+        $ do
+              origin <- range @Double
+              v      <- choose origin
+              a      <- arbitrary
+              return (a === normalise origin (a, a) v)
+
+    prop
+            "if the origin has no range, then there is an error because it's impossible to determine what it should be the in the target range"
+        $ do
+              target <- range @Double
+              v      <- choose target
+              a      <- arbitrary
+              let
+                  outcome =
+                      evaluateNF (normalise (a, a) target v)
+                          `shouldThrow` anyErrorCall
+              return outcome
+
+    prop
+            "if the origin has no range, then there is an error because it's impossible to determine what it should be the in the target range"
+        $ do
+              (lowEnd, highEnd) <- range @Double
+              target <- range @Double
+              value <- arbitrary `suchThat` \n -> lowEnd > n || highEnd < n
+              let outcome =
+                      evaluateNF (normalise (lowEnd, highEnd) target value)
+                          `shouldThrow` anyErrorCall
+              return outcome
